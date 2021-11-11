30f883a4b62054599819c1a18ea0cad8f45a1618
Đỗ Huy Hoàng <huyhoang14901@gmail.com>
2021-11-11 18:08:44 +0700

    Xử lý loading GD_DangNhap

--- src/main/java/gui/GD_DangNhap.java
+++ src/main/java/gui/GD_DangNhap.java
@@ -2,10 +2,21 @@ package gui;
 
 import entity.NhanVien;
 import gui.component.Message;
+import gui.component.PanelForm;
+import gui.component.PanelLoading;
 import gui.swing.event.EventOnClick;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.Path2D;
+import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
 import org.jdesktop.animation.timing.Animator;
 import org.jdesktop.animation.timing.TimingTarget;
 import org.jdesktop.animation.timing.TimingTargetAdapter;
@@ -13,10 +24,102 @@ import org.jdesktop.animation.timing.TimingTargetAdapter;
 public class GD_DangNhap extends javax.swing.JFrame {
 
     private boolean show = true;
+    private Animator animator;
+    private float animate = 1f;
+    private boolean slideLeft;
+    private PanelForm login;
+    private PanelLoading loading;
+    private Thread th;
+    private MigLayout layout;
 
     public GD_DangNhap(String title) {
         super(title);
         initComponents();
+
+    }
+
+    private void buildDisplay() {
+        layout = new MigLayout("inset 0", "[fill]", "[fill]");
+        setLayout(layout);
+        login = new PanelForm();
+        loading = new PanelLoading();
+        loading.setVisible(false);
+        Color color = new Color(67, 99, 132);
+        setBackground(color);
+        loading.setBackground(color);
+        TimingTarget target = new TimingTargetAdapter() {
+            @Override
+            public void begin() {
+                if (slideLeft) {
+                    loading.setVisible(true);
+                } else {
+                    login.setVisible(true);
+                }
+            }
+
+            @Override
+            public void timingEvent(float fraction) {
+                double width = getWidth();
+                animate = fraction;
+                float a = easeOutQuint(fraction);
+                int x = (int) (a * width);
+                loading.setAnimate(slideLeft, fraction);
+                layout.setComponentConstraints(loading, "pos " + x + " 0 100% 100%");
+                revalidate();
+                repaint();
+            }
+
+            @Override
+            public void end() {
+                if (slideLeft) {
+                    login.setVisible(false);
+                } else {
+                    loading.setVisible(false);
+                    loading.reset();
+                }
+            }
+        };
+        animator = new Animator(1000, target);
+        animator.setResolution(0);
+        add(loading, " pos 0 0 0 0, w 0!");
+        add(login);
+        login.login(new EventOnClick() {
+            @Override
+            public void onClick(Object object) {
+                if (!animator.isRunning()) {
+                    showSlide(true);
+                    NhanVien nhanVien = (NhanVien) object;
+                    if (nhanVien != null) {
+                        GD_DangNhap.this.dispose();
+                        new GD_Chinh("Quản lý Karaoke FourH", nhanVien).setVisible(true);
+                    } else {
+                        login.showMessage(Message.MessageType.ERROR, "Sai mật khẩu");
+                    }
+                }
+            }
+        });
+        loading.addEventContinue(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                Main main = new Main();
+                main.setData(loading.getData());
+                main.setVisible(true);
+                //  Close login form
+                GD_DangNhap.this.dispose();
+            }
+        });
+        loading.addEventBack(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                if (!animator.isRunning()) {
+                    th.interrupt();
+                    showSlide(false);
+                }
+            }
+        });
+    }
+
+    private void start() {
         setBackground(new Color(0, 0, 0, 0));
         setOpacity(0);
         TimingTarget target = new TimingTargetAdapter() {
@@ -51,18 +154,12 @@ public class GD_DangNhap extends javax.swing.JFrame {
     }
 
     private void login() {
-        pnlForm.login(new EventOnClick() {
-            @Override
-            public void onClick(Object object) {
-                NhanVien nhanVien = (NhanVien) object;
-                if (nhanVien != null) {
-                    GD_DangNhap.this.dispose();
-                    new GD_Chinh("Quản lý Karaoke FourH", nhanVien).setVisible(true);
-                } else {
-                    pnlForm.showMessage(Message.MessageType.ERROR, "Sai mật khẩu");
-                }
-            }
-        });
+
+    }
+
+    public void showSlide(boolean show) {
+        slideLeft = show;
+        animator.start();
     }
 
     @SuppressWarnings("unchecked")
@@ -70,7 +167,6 @@ public class GD_DangNhap extends javax.swing.JFrame {
     private void initComponents() {
 
         bg = new gui.swing.panel.LayerPaneShadow();
-        pnlForm = new gui.component.PanelForm();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -80,67 +176,47 @@ public class GD_DangNhap extends javax.swing.JFrame {
         bg.setShadowOpacity(0.2F);
         bg.setShadowSize(15);
 
-        bg.setLayer(pnlForm, javax.swing.JLayeredPane.DEFAULT_LAYER);
-
-        javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
-        bg.setLayout(bgLayout);
-        bgLayout.setHorizontalGroup(
-            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pnlForm, javax.swing.GroupLayout.DEFAULT_SIZE, 790, Short.MAX_VALUE)
-        );
-        bgLayout.setVerticalGroup(
-            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pnlForm, javax.swing.GroupLayout.DEFAULT_SIZE, 481, Short.MAX_VALUE)
-        );
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(bg, javax.swing.GroupLayout.PREFERRED_SIZE, 762, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(bg, javax.swing.GroupLayout.PREFERRED_SIZE, 452, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GD_DangNhap.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GD_DangNhap.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GD_DangNhap.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GD_DangNhap.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    @Override
+    public void paint(Graphics grphcs) {
+        super.paint(grphcs);
+        if (slideLeft == false) {
+            Graphics2D g2 = (Graphics2D) grphcs.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            int width = getWidth();
+            int height = getHeight();
+            float x = easeOutQuint(animate) * width;
+            float y = 0;
+            int centerY = height / 2;
+            Path2D.Float p = new Path2D.Float();
+            p.moveTo(x, y);
+            p.lineTo(x, height);
+            p.curveTo(x, height, easeOutBounce(animate) * width, centerY, x, y);
+            g2.setColor(getBackground());
+            g2.fill(p);
+            g2.dispose();
         }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GD_DangNhap("Đăng nhập").setVisible(true);
-            }
-        });
     }
 
-    public float easeOutBounce(float x) {
+    private float easeOutBounce(float x) {
         float n1 = 7.5625f;
         float d1 = 2.75f;
         double v;
@@ -153,11 +229,23 @@ public class GD_DangNhap extends javax.swing.JFrame {
         } else {
             v = n1 * (x -= 2.625 / d1) * x + 0.984375;
         }
-        return (float) v;
+        if (slideLeft) {
+            return (float) (1f - v);
+        } else {
+            return (float) v;
+        }
     }
-    
+
+    private float easeOutQuint(float x) {
+        double v = 1 - Math.pow(1 - x, 5);
+        if (slideLeft) {
+            return (float) (1f - v);
+        } else {
+            return (float) v;
+        }
+    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private gui.swing.panel.LayerPaneShadow bg;
-    private gui.component.PanelForm pnlForm;
     // End of variables declaration//GEN-END:variables
 }
--- src/main/java/gui/swing/panel/PanelTransparent.java
+++ src/main/java/gui/swing/panel/PanelTransparent.java
@@ -3,32 +3,24 @@ package gui.swing.panel;
 import java.awt.AlphaComposite;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import javax.swing.JComponent;
+import javax.swing.JPanel;
 
-public class PanelTransparent extends JComponent {
-    
-    public float getAlpha() {
-        return alpha;
-    }
-    
+public class PanelTransparent extends JPanel {
+
     public void setAlpha(float alpha) {
         this.alpha = alpha;
-        repaint();
     }
-    
-    private float alpha;
-    
+
+    private float alpha = 1f;
+
+    public PanelTransparent() {
+        setOpaque(false);
+    }
+
     @Override
     public void paint(Graphics grphcs) {
-        Graphics2D g2 = (Graphics2D) grphcs.create();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(getBackground());
+        Graphics2D g2 = (Graphics2D) grphcs;
         g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-        int x[] = {0, getWidth(), getWidth() - 100, 0};
-        int y[] = {0, 0, getHeight(), getHeight()};
-        g2.fillPolygon(x, y, x.length);
-        g2.dispose();
         super.paint(grphcs);
     }
 }
